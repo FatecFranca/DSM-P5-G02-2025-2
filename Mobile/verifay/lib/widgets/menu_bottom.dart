d55ae5f52facf0c6124e099d0c5bebb0b237da47
InacioSant <inaciosantana231@gmail.com>
2025-11-11 19:21:36 -0300

    feat: Adicionar navegação para páginas de Detecção e Histórico no menu

--- Mobile/verifay/lib/widgets/menu_bottom.dart
+++ Mobile/verifay/lib/widgets/menu_bottom.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../pages/dashboard_page.dart';
+import '../pages/detection_page.dart';
+import '../pages/history_page.dart';
 
 class NavigationMenu extends StatefulWidget {
   const NavigationMenu({super.key});
@@ -11,11 +13,18 @@ class NavigationMenu extends StatefulWidget {
 class _NavigationMenuState extends State<NavigationMenu> {
   int _selectedIndex = 0;
 
-  static final List<Widget> _pages = <Widget>[
-    DashboardPage(),
-    Center(child: Text('teste1')),
-    Center(child: Text('teste2')),
+  List<Widget> get _pages => [
+    DashboardPage(
+      onNavigate: (index) {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+    ),
+    DetectionPage(),
+    HistoryPage(),
   ];
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -36,17 +45,24 @@ class _NavigationMenuState extends State<NavigationMenu> {
             color: Colors.white,
           ),
         ),
-        
-        
-
         height: 80,
         elevation: 0,
         selectedIndex: _selectedIndex,
         onDestinationSelected: _onItemTapped,
         destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: "Início", selectedIcon: Icon(Icons.home_outlined),),
-          NavigationDestination(icon: Icon(Icons.chat_rounded), label: "Chat"),
-          NavigationDestination(icon: Icon(Icons.settings), label: "Teste"),
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: "Início",
+            selectedIcon: Icon(Icons.home_outlined),
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.chat_rounded),
+            label: "Detecção",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.punch_clock_outlined),
+            label: "Historico",
+          ),
         ],
       ),
       body: _pages[_selectedIndex],
